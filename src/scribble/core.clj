c3b059f06146ad123cb8c75b3241868bc2583e56
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-04 21:54:50 +1000

    src/scribble/core: added some comments

--- src/scribble/core.clj
+++ src/scribble/core.clj
@@ -1,3 +1,16 @@
+;; The syntax differs from the Racket's Scribble in the following ways:
+;;
+;; - `@;` stands for a simple single-line comment,
+;;   which just consumes everything until `\newline`.
+;; - `@;;` does what `@;` does in Scribble, i.e. consumes everything
+;;   until `\newline`, and then until the next non-whitespace character or the second `\newline`.
+;; - `{}` reads as a vector of string and nested forms
+;;   and is passed as a single argument to the function,
+;;   e.g. `@foo{bar @baz{blah}}` reads as `(foo ["bar " (baz ["blah"])])`.
+;; - Any number of `[]` and `{}` groups in any order is allowed in the Scribble form
+;;   (provided that they are not separated by whitespace),
+;;   e.g. `@foo[:bar 2]{baz}[:blah 3]` reads as `(foo :bar 2 ["baz"] :blah 3)`.
+
 (ns scribble.core
   (:use [clarity.reader.hacking :only [with-reader-macro]])
   (:use [clarity.reader.macros :only [use-reader-macros]])
@@ -96,7 +109,10 @@
         ; reading characters
         :else (recur vec-accum (conj str-accum c) false newline-encountered)))))
 
-(defn text-postprocess [raw-vec starting-indent]
+(defn text-postprocess
+  "Removes excessive whitespace according to the following rules:
+  - if the vector starts  "
+  [raw-vec starting-indent]
   raw-vec)
 
 (defn scribble-normal-reader [reader]
@@ -145,7 +161,9 @@
         (= \newline c) (recur true)
         :else (recur newline-encountered)))))
 
-(defn scribble-entry-reader [reader _]
+(defn scribble-entry-reader
+  "The entry point of the reader macro."
+  [reader _]
   (println "- In scribble-entry-reader")
   (let [c (my-peek reader)]
     (condp = c
@@ -158,8 +176,12 @@
         (println "-- entry-reader finished " (repr (cons sym forms)))
         (cons sym forms)))))
 
-(defn use-scribble []
+(defn use-scribble
+  "Enables the Scribble reader macro in the current namespace."
+  []
   (use-reader-macros {:char scribble-char :reader scribble-entry-reader}))
 
-(defmacro with-scribble [& forms]
+(defmacro with-scribble
+  "Temporarily enables the Scribble reader macro."
+  [& forms]
   `(with-reader-macro scribble-char scribble-entry-reader (do ~@forms)))
