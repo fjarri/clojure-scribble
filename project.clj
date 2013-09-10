d6ee4a2d464bdb0a42af51341a6053aaec127d03
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-10 23:14:33 +1000

    project: put plugins in the dev profile and add test_resources folder

--- project.clj
+++ project.clj
@@ -5,9 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [one_more_minute/clarity "0.1.1"]]
-  :plugins [
-    [lein-marginalia "0.7.1"] ; build docs, 'lein marg'
-    [lein-cloverage "1.0.2"] ; run tests and check coverage, 'lein cloverage'
-    [lein-checkall "0.1.1"] ; check code style, 'lein checkall'
-    [perforate "0.3.3"]] ; run performance tests, 'lein perforate'
+  :profiles {
+    :dev {
+      :plugins [
+        [lein-marginalia "0.7.1"] ; build docs, 'lein marg'
+        [lein-cloverage "1.0.2"] ; run tests and check coverage, 'lein cloverage'
+        [lein-checkall "0.1.1"] ; check code style, 'lein checkall'
+        [perforate "0.3.3"]] ; run performance tests, 'lein perforate'
+      :resource-paths ["test_resources"]
+      }}
   :main scribble.core)
