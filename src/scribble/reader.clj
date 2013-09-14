de14919fd96d4d545b733040cf0950d96c4db0ed
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-14 16:59:43 +1000

    src/reader: fixed the logic for nested verbatim forms

--- src/scribble/reader.clj
+++ src/scribble/reader.clj
@@ -58,6 +58,19 @@
   [v]
   (vec (map inverse-char (reverse v))))
 
+(defn here-markers
+  [here-str]
+  (if (nil? here-str)
+    [[] []]
+    [
+      (str scribble-verbatim-start here-str)
+      (str scribble-text-end (clojure.string/join (inverse-vec (vec here-str))))]))
+
+(defn str-pop
+  [v n]
+  (if (zero? n)
+    v
+    (subvec v 0 (- (count v) n))))
 
 (declare scribble-entry-reader)
 
@@ -65,52 +78,72 @@
   "Returns a vector of strings and nested forms.
   The strings are separated as [leading whitespace, contents, trailing whitespace, newline]
   (for the ease of further processing)."
-  [reader escaped]
+  [reader here-str]
+  (let [[here-start here-end] (here-markers here-str)
+        here-marker-len (count here-start)
+        escaped (not (nil? here-str))]
   (loop [text-accum []
          str-accum []
          ; FIXME: using a custom type will be faster
          state {:leading-ws true
                 :brace-level 0
-                :escaped-char false}]
+                :here-str-pos 0}]
     (let [c (reader-methods/read-1 reader)]
       (cond
 
+        (and escaped (= c scribble-verbatim-end) (= (- (:here-str-pos state)) here-marker-len))
+          (if (zero? (:brace-level state))
+            (dump-string text-accum (str-pop str-accum here-marker-len))
+            (recur text-accum (conj str-accum c)
+              (assoc state
+                :brace-level (dec (:brace-level state))
+                :here-str-pos 0)))
+
         (and escaped (= c scribble-verbatim-start))
-          (let [next-c (reader-methods/peek reader)]
-            (condp = next-c
-              scribble-char
-                (recur text-accum str-accum
-                  (assoc state :escaped-char true))
-              scribble-text-start
-                (recur text-accum (conj str-accum c)
-                  (assoc state :escaped-char true))
-              (recur text-accum (conj str-accum c) state)))
+          (let [[text-accum str-accum] (if (:leading-ws state)
+                                         [(dump-leading-ws text-accum str-accum) [c]]
+                                         [text-accum (conj str-accum c)])]
+              (recur text-accum str-accum
+                (assoc state
+                  :here-str-pos 1
+                  :leading-ws false)))
 
-        (and escaped (= c scribble-text-start) (:escaped-char state))
+        (and escaped (= c scribble-text-end))
+          (let [[text-accum str-accum] (if (:leading-ws state)
+                                         [(dump-leading-ws text-accum str-accum) [c]]
+                                         [text-accum (conj str-accum c)])]
+              (recur text-accum str-accum
+                (assoc state
+                  :here-str-pos -1
+                  :leading-ws false)))
+
+        (and escaped (= c scribble-text-start) (= (:here-str-pos state) here-marker-len))
           (recur text-accum (conj str-accum c)
-            (assoc state :escaped-char false :brace-level (inc (:brace-level state))))
+            (assoc state :here-str-pos 0 :brace-level (inc (:brace-level state))))
 
-        (and escaped (= c scribble-text-end) (= (reader-methods/peek reader) scribble-verbatim-end))
-          (let [next-c (reader-methods/read-1 reader)]
-            (if (zero? (:brace-level state))
-              (if (:leading-ws state)
-                (dump-leading-ws text-accum str-accum)
-                (dump-string text-accum str-accum))
-              (if (:leading-ws state)
-                (recur (dump-leading-ws text-accum str-accum) [c next-c]
-                  (assoc state :leading-ws false :brace-level (dec (:brace-level state))))
-                (recur text-accum (-> str-accum (conj c) (conj next-c))
-                  (update-in state [:brace-level] dec)))))
-
-        (and escaped (= c scribble-char) (:escaped-char state))
+        (and escaped (= c scribble-char) (= (:here-str-pos state) here-marker-len))
           (let [nested-form (scribble-entry-reader reader c)
+                str-accum (str-pop str-accum here-marker-len)
                 [text-accum str-accum]
                   (if (identical? nested-form reader)
                     [text-accum str-accum]
                     (dump-nested-form text-accum str-accum nested-form (:leading-ws state)))]
             (recur text-accum str-accum
-              (assoc state :leading-ws false :escaped-char false)))
+              (assoc state :here-str-pos 0)))
 
+        (and escaped
+            (> (:here-str-pos state) 0)
+            (< (:here-str-pos state) here-marker-len)
+            (= c (nth here-start (:here-str-pos state))))
+          (recur text-accum (conj str-accum c)
+            (update-in state [:here-str-pos] inc))
+
+        (and escaped
+            (< (:here-str-pos state) 0)
+            (< (- (:here-str-pos state)) here-marker-len)
+            (= c (nth here-end (- (:here-str-pos state)))))
+          (recur text-accum (conj str-accum c)
+            (update-in state [:here-str-pos] dec))
 
 
         ; Starting text-mode symbol
@@ -154,22 +187,26 @@
                  (-> text-accum
                    (dump-string str-accum)
                    append-newline)]
-            (recur text-accum [] (assoc state :leading-ws true)))
+            (recur text-accum [] (assoc state :leading-ws true :here-str-pos 0)))
 
         ; in leading whitespace mode, whitespace character encountered
         (and (whitespace? c) (:leading-ws state))
-          (recur text-accum (conj str-accum c) (assoc state :leading-ws true))
+          (recur text-accum (conj str-accum c) (assoc state :leading-ws true :here-str-pos 0))
         (:leading-ws state)
-          (recur (dump-leading-ws text-accum str-accum) [c] (assoc state :leading-ws false))
+          (recur (dump-leading-ws text-accum str-accum) [c]
+            (assoc state :leading-ws false :here-str-pos 0))
 
         ; reading characters
-        :else (recur text-accum (conj str-accum c) state)))))
+        :else (recur text-accum (conj str-accum c)
+          (assoc state :here-str-pos 0)))))))
 
 (defn scribble-text-block-reader
-  [reader escaped]
+  [reader here-str]
+  ; FIXME: check that here-str does not contain verbatim start/end chars,
+  ; scribble char, or text start/end chars
   (let [[_ c] (reader-methods/reader-position reader)
         column (if (nil? c) 0 c)
-        text-accum (scribble-text-reader reader escaped)
+        text-accum (scribble-text-reader reader here-str)
         text-form (text-postprocess text-accum column)]
     text-form))
 
@@ -220,11 +257,14 @@
         (and (= c scribble-verbatim-start) (= (reader-methods/peek reader) scribble-text-start))
           (do
             (reader-methods/read-1 reader)
-            (recur (conj forms-read (scribble-text-block-reader reader true)) true))
+            (recur (conj forms-read (scribble-text-block-reader reader "")) true))
         (= c scribble-verbatim-start)
-          (recur (conj forms-read (scribble-verbatim-reader reader)) true)
+          (let [s (read-until reader #(= % scribble-text-start))
+                _ (reader-methods/read-1 reader)
+                verbatim-form (scribble-text-block-reader reader s)]
+            (recur (conj forms-read verbatim-form) true))
         (= c scribble-text-start)
-          (recur (conj forms-read (scribble-text-block-reader reader false)) true)
+          (recur (conj forms-read (scribble-text-block-reader reader nil)) true)
         (= c scribble-normal-start)
           (let [forms (reader-methods/read-delimited-list scribble-normal-end reader)]
             (recur (vec (concat forms-read forms)) true))
@@ -302,9 +342,10 @@
       (= c scribble-verbatim-start)
         (let [s (read-until reader #(or (= % scribble-verbatim-end) (= % scribble-text-start)))
               c (reader-methods/peek reader)]
+          (reader-methods/read-1 reader)
           (if (= c scribble-verbatim-end)
-            (do (reader-methods/read-1 reader) (try-recognize-symbol reader s))
-            (let [verbatim-form (scribble-verbatim-read reader s)
+            (try-recognize-symbol reader s)
+            (let [verbatim-form (scribble-text-block-reader reader s)
                   forms (scribble-form-reader reader)]
               (cond
                 (identical? reader forms) (list verbatim-form)
