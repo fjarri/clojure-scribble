3d0f693547480218a9b135bc7c7e260136874286
Mike Innes <mike.j.innes@gmail.com>
2013-09-14 01:59:04 +0100

    update to clarity to chiara 0.2.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [one_more_minute/clarity "0.1.1"]
+                 [chiara "0.2.0-SNAPSHOT"]
                  [org.clojure/tools.reader "0.7.6"]]
   :profiles {
     :dev {
