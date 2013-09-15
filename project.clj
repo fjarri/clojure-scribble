6d3a82ced0e10c00b9d33fbb51150b830e8062d2
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-16 01:03:32 +1000

    Rolling back brevious commit.
    We also depend on reader-error.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.reader "0.7.6"]
                  [chiara "0.2.0-SNAPSHOT"]]
   :profiles {
     :dev {
--- src/scribble/symbol.clj
+++ src/scribble/symbol.clj
@@ -3,7 +3,8 @@
 ;; Unfortunately, it is private and coupled to `reader`
 ;; (which makes it difficult to work with escaped symbols),
 ;; that's why we are not importing it.
-(ns scribble.symbol)
+(ns scribble.symbol
+  (:use [clojure.tools.reader.impl.commons :only [parse-symbol]]))
 
 ;; A simple wrapper for a symbol returned by `recognize-symbol`,
 ;; because that symbol can be nil, and we can't even wrap it in a list.
@@ -12,27 +13,6 @@
 (defn unwrap-symbol
   [^WrappedSymbol wrapped-sym]
   (.sym wrapped-sym))
-
-(defn parse-symbol
-  "Copy of `parse-symbol` from `clojure.tools.reader.impl.commons`,
-  with `numeric?` replaced by `.isDigit`."
-  [^String token]
-  (when-not (or (identical? "" token)
-                (not= -1 (.indexOf token "::")))
-    (let [ns-idx (.indexOf token "/")]
-      (if-let [ns (and (pos? ns-idx)
-                       (subs token 0 ns-idx))]
-        (let [ns-idx (inc ns-idx)]
-          (when-not (== ns-idx (count token))
-            (let [sym (subs token ns-idx)]
-              (when (and (not (Character/isDigit (.charAt sym 0)))
-                         (not (identical? "" sym))
-                         (or (= sym "/")
-                             (== -1 (.indexOf sym "/"))))
-                [ns sym]))))
-        (when (or (= token "/")
-                  (== -1 (.indexOf token "/")))
-            [nil token])))))
 
 (defn recognize-symbol
   "Modified `read-symbol` from `clojure.tools.reader.impl.commons`.
