8bda809549537dbc657e737ea936f6c7f2c850ca
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-18 00:27:40 +1000

    core_benchmark: fixed to run with the recent changes to the symbol reading

--- benchmarks/scribble/core_benchmark.clj
+++ benchmarks/scribble/core_benchmark.clj
@@ -1,11 +1,25 @@
 (ns scribble.core-benchmark
   (:require [clojure.java.io :as io])
   (:use perforate.core)
-  (:use scribble.core))
+  (:use [chiara.reader.hacking :only [set-reader-macro]])
+  (:use scribble.core)
+  (:use scribble.settings))
+
+; Making `|` character macro-terminating,
+; so that the test file could be read properly.
+; FIXME: changes the macro table globally!
+; Should be replaced by something safer, e.g.:
+; - with-reader-macros (when it's available from chiara),
+; - or if we start to check for non-terminating characters
+;   on settings creation and set stub readers for them ourselves
+;   (somewhere in with-scribble-settings maybe).
+(defn- reader-stub [reader _] reader)
+(set-reader-macro \| reader-stub)
 
 (defgoal reader-bench "Reader benchmark.")
 
 (defcase reader-bench :scribble-doc-load
   []
-  (with-scribble
-    (load-file (.getPath (io/resource "doc.txt")))))
+  (with-scribble-settings
+    (make-settings \@ \{ \} \[ \] \| \| \;)
+      (load-file (.getPath (io/resource "doc.txt")))))
