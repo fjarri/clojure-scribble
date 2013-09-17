6aed878a909122ab00f4aec52194ab520d147303
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-17 23:11:36 +1000

    src/core: fixed escaping characters

--- src/scribble/core.clj
+++ src/scribble/core.clj
@@ -13,7 +13,7 @@
 ;; the parent Scribble form anymore.
 ;; Every body form is thus wrapped in a vector (see the example above).
 ;; Another consequence is that `@foo{}` reads as `(foo [])` and not as `foo`
-;; (if you want the latter, you may use just `@foo` or escaped `@`foo``).
+;; (if you want the latter, you may use just `@foo` or escaped `` @`foo` ``).
 ;;
 ;; The next difference is caused by the implementation.
 ;; Any reader macros that follow the `@` are *not* applied to the whole
@@ -23,13 +23,13 @@
 ;; and uses the resulting form as a command.
 ;;
 ;; Since the same symbol is used for spliced commands and for escaped string,
-;; it is impossible to distinguish whether `@`-` starts a list of forms
-;; (e.g. `@`- 3 2``), or an escaped string (like `@`-{blah}-``).
+;; it is impossible to distinguish whether `` @`- `` starts a list of forms
+;; (e.g. `` @`- 3 2` ``), or an escaped string (like `` @`-{blah}-` ``).
 ;; Therefore the escaped strings at the start of a Scribble form must
-;; use two escaping characters: `@``-{blah}-``.
+;; use two escaping characters: ``` @``-{blah}-` ```.
 ;;
 ;; Following from the previous difference, the empty spliced command must
-;; contain at least one whitespace symbol: `@foo{Alice@` `Bob}`.
+;; contain at least one whitespace symbol: ``@foo{Alice@` `Bob}``.
 ;;
 ;; Finally, `@;` consumes characters only till the newline;
 ;; the newline-consuming comment is written as `@;;`.
