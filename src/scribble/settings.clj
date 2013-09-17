a048d80d7c1bf110a8106e8c4a95a120b85be5e0
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-18 01:01:08 +1000

    src/settings: removed unused functions, added comments

--- src/scribble/settings.clj
+++ src/scribble/settings.clj
@@ -1,17 +1,12 @@
+;; Contains functions that allow you to tune the characters used for
+;; different purposes in the reader.
 (ns scribble.settings)
 
-(defn whitespace? [c]
+
+(defn whitespace?
+  [c]
   (or (= c \space) (= c \tab)))
 
-(def ^:private clojure-symbol-start-chars
-  ; according to http://clojure.org/reader (as of 1.5.1)
-  #{\* \+ \! \- \_ \? \/ \: \.})
-
-(defn clojure-symbol-start?
-  [^Character c]
-  (or
-    (Character/isLetter c)
-    (contains? clojure-symbol-start-chars c)))
 
 (deftype Settings [
   ^Character entry-char
@@ -21,10 +16,41 @@
   ^Character datum-end-char
   ^Character escape-start-char
   ^Character escape-end-char
-  ^Character comment-char
-  symbol-end?])
+  ^Character comment-char])
+
+(defn- validate-settings
+  "Checks the reader settings for conflicts."
+  [^Settings settings]
+  settings)
 
 (defn make-settings
+  "Creates a reader settings structure,
+  and checks the characters used for conflicts.
+  Parameters:
+
+  - `entry-char`: the entry point to our custom reader.
+    **Warning:** any default reader macro for this symbol will be unusable
+    when Scribble is enabled.
+  - `body-start-char`, `body-end-char`: mark the body part.
+    Ideally these occur rarely in the text, otherwise a lot of escaping
+    will have to be used.
+  - `datum-start-char`, `datum-end-char`: mark the datum part.
+    `datum-end-char` must be a macro-terminating character
+    (if it's not, it has to be preceeded by whitespace
+    when it's used to terminate the datum part).
+  - `escape-start-char`, `escape-end-char`: used for escaping body part
+    and splicing forms.
+    If `escape-start-char` has a reader macro assigned, this macro will be
+    unusable in the first form of the splice (because two escaping characters
+    mark the beginning of an escaped body part).
+    `escape-end-char` must be a macro-terminating character
+    (if it's not, it has to be preceeded by whitespace
+    when it's used to terminate the spliced form).
+  - `comment-char` is used (with a preceding `entry-char`)
+    to start line comments, newline-consuming comments, and multiline comments.
+
+  Characters in every group may be equal, but should differ from all
+  characters in other groups."
   [entry-char
    body-start-char
    body-end-char
@@ -33,18 +59,7 @@
    escape-start-char
    escape-end-char
    comment-char]
-  (let [symbol-end-chars
-         (set [entry-char
-               body-start-char
-               body-end-char
-               datum-start-char
-               datum-end-char
-               escape-start-char
-               escape-end-char
-               \space
-               \tab
-               \newline])
-        symbol-end? (fn [^Character c] (contains? symbol-end-chars c))]
+  (validate-settings
     (Settings. entry-char
                body-start-char
                body-end-char
@@ -52,18 +67,18 @@
                datum-end-char
                escape-start-char
                escape-end-char
-               comment-char
-               symbol-end?)))
+               comment-char)))
 
 (def default-settings (make-settings \@ \{ \} \[ \] \` \` \;))
 
+
 (defn entry-char
-  [^Settings settings-]
-  (.entry-char settings-))
+  [^Settings settings]
+  (.entry-char settings))
 
 (defn inverse-char
   [c]
-  (condp = c
+  (case c
     \( \)
     \) \(
     \[ \]
