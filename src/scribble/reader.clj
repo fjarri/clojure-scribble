a8b8b5ebdae3bd2e6a4c03f40f4f304894ef6250
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-20 00:27:19 +1000

    src/reader: added some comments

--- src/scribble/reader.clj
+++ src/scribble/reader.clj
@@ -1,3 +1,4 @@
+;; Contains the main reader macro and readers for nested special forms.
 (ns scribble.reader
   (:use [clojure.set :only [intersection]])
   (:require [chiara.reader.utils :as reader-methods]
@@ -21,26 +22,35 @@
 
 
 (defn- here-markers
+  "Creates beginning and ending here-string markers
+  for usage in the body part reader."
   [here-str]
   (if (nil? here-str)
     [[] []]
-    [
-      (str \space here-str)
-      (str \space (inverse-str here-str))]))
+    ; Additional symbols allow us to avoid `dec`s in comparisons.
+    [(str \space here-str)
+     (str \space (inverse-str here-str))]))
 
+;; The following two functions could be joined into one,
+;; but that would require creation and destructuring of a vector
+;; `[body-accum str-accum]`, and which noticeably affects
+;; `read-body` performance.
 
 (defn- dump-nonws-char-body
+  "Updates a body part accumulator
+  when a non-whitespace character is encountered"
   [body-accum str-accum leading-ws]
   (if leading-ws
     (dump-leading-ws body-accum str-accum)
     body-accum))
 
 (defn- dump-nonws-char-str
+  "Updates a string accumulator
+  when a non-whitespace character is encountered"
   [str-accum c leading-ws]
   (if leading-ws
     (make-str-accum c)
     (str-accum-push str-accum c)))
-
 
 (declare read-entry)
 
@@ -58,127 +68,152 @@
         body-end-char (.body-end-char settings)
         escape-start-char (.escape-start-char settings)
         escape-end-char (.escape-end-char settings)]
-  (loop [body-accum []
-         str-accum (make-str-accum)
-         leading-ws true
-         brace-level (int 0)
-         here-str-pos (int 0)]
-    (let [c (reader-methods/read-1 reader)]
-      (cond
+    (loop [body-accum []
+           str-accum (make-str-accum)
+           ; Indicates the leading whitespace reading mode.
+           leading-ws true
+           ; Using privitive integers gets us some speed-up.
+           brace-level (int 0)
+           ; If the position is positive, we are reading the starting marker,
+           ; if it is negative, we are reading the ending marker.
+           here-str-pos (int 0)]
+      (let [c (reader-methods/read-1 reader)]
+        (cond
 
-        (or (and escaped
-                 (= c escape-end-char)
-                 (== (- here-str-pos) here-marker-len))
-            (and (not escaped)
-                 (= c body-end-char)))
-          (if (zero? brace-level)
-            (if leading-ws
-              (dump-leading-ws body-accum str-accum)
-              (dump-string body-accum (str-accum-pop str-accum here-marker-len)))
+          ; Catches the body part ending character
+          ; (or the escape ending one, if we are in escaped mode)
+          (or (and escaped
+                   (= c escape-end-char)
+                   (== (- here-str-pos) here-marker-len))
+              (and (not escaped)
+                   (= c body-end-char)))
+            (if (zero? brace-level)
+              (if leading-ws
+                (dump-leading-ws body-accum str-accum)
+                (dump-string
+                  body-accum
+                  (str-accum-pop str-accum here-marker-len)))
+              (recur
+                (dump-nonws-char-body body-accum str-accum leading-ws)
+                (dump-nonws-char-str str-accum c leading-ws)
+                false
+                (dec brace-level)
+                (int 0)))
+
+          ; Catches the body part starting character
+          ; (or the escape starting one, if we are in escaped mode)
+          (or (and escaped
+                   (= c escape-start-char))
+              (and (= c body-start-char)
+                   (== here-str-pos here-marker-len)))
             (recur
               (dump-nonws-char-body body-accum str-accum leading-ws)
               (dump-nonws-char-str str-accum c leading-ws)
               false
-              (dec brace-level)
-              (int 0)))
+              ; The body-start-char will be at the end of the here-string
+              ; sequence in escaped mode, so we can safely increase
+              ; the brace level regardless of `escaped`.
+              (if (= c body-start-char)
+                (inc brace-level)
+                brace-level)
+              ; If we are in escaped mode and caught the escape starting,
+              ; just increase the here-string position.
+              (if (= c escape-start-char)
+                (int 1)
+                (int 0)))
 
-        (or (and escaped
-                 (= c escape-start-char))
-            (and (= c body-start-char)
-                 (== here-str-pos here-marker-len)))
-          (recur
-            (dump-nonws-char-body body-accum str-accum leading-ws)
-            (dump-nonws-char-str str-accum c leading-ws)
-            false
-            (if (= c body-start-char)
-              (inc brace-level)
-              brace-level)
-            (if (= c escape-start-char)
-              (int 1)
-              (int 0)))
-
-        (and escaped
-             (= c body-end-char))
-          (recur
-            (dump-nonws-char-body body-accum str-accum leading-ws)
-            (dump-nonws-char-str str-accum c leading-ws)
-            false
-            brace-level
-            (int -1))
-
-
-        (and (= c entry-char)
-             (== here-str-pos here-marker-len))
-          (let [nested-form (read-entry settings reader c)
-                str-accum (str-accum-pop str-accum here-marker-len)
-                [body-accum str-accum]
-                  (if (identical? nested-form reader)
-                    [body-accum str-accum]
-                    (dump-nested-form
-                      body-accum str-accum nested-form leading-ws))]
+          ; In escaped mode, body-end-char only means that
+          ; we should start comparing the following characters with
+          ; the ending here-string marker.
+          (and escaped
+               (= c body-end-char))
             (recur
-              body-accum
-              str-accum
+              (dump-nonws-char-body body-accum str-accum leading-ws)
+              (dump-nonws-char-str str-accum c leading-ws)
               false
               brace-level
-              (int 0)))
+              (int -1))
 
-        (and (pos? here-str-pos)
-             (< here-str-pos here-marker-len)
-             (= c (nth here-start here-str-pos)))
-          (recur
-            body-accum
-            (str-accum-push str-accum c)
-            leading-ws
-            brace-level
-            (inc here-str-pos))
+          ; Entry character encountered,
+          ; and we are at the end of the starting here-string marker.
+          (and (= c entry-char)
+               (== here-str-pos here-marker-len))
+            (let [nested-form (read-entry settings reader c)
+                  ; Pop here-string marker from the accumulator
+                  str-accum (str-accum-pop str-accum here-marker-len)
+                  [body-accum str-accum]
+                    ; Check if the nested form was a comment
+                    (if (identical? nested-form reader)
+                      [body-accum str-accum]
+                      (dump-nested-form
+                        body-accum str-accum nested-form leading-ws))]
+              (recur
+                body-accum
+                str-accum
+                false
+                brace-level
+                (int 0)))
 
-        (and (neg? here-str-pos)
-             (< (- here-str-pos) here-marker-len)
-             (= c (nth here-end (- here-str-pos))))
-          (recur
-            body-accum
-            (str-accum-push str-accum c)
-            leading-ws
-            brace-level
-            (dec here-str-pos))
-
-        ; unexpected EOF
-        (nil? c)
-          (reader-error reader "Unexpected EOF while reading a body part")
-
-        ; newline encountered: dump accumulator,
-        ; turn the leading whitespace mode on
-        (= c \newline)
-          (let [body-accum
-                 (-> body-accum
-                   (dump-string str-accum)
-                   append-newline)]
+          ; Reading the starting here-string marker,
+          ; and the current character is correct.
+          (and (pos? here-str-pos)
+               (< here-str-pos here-marker-len)
+               (= c (nth here-start here-str-pos)))
             (recur
               body-accum
-              (make-str-accum)
+              (str-accum-push str-accum c)
+              leading-ws
+              brace-level
+              (inc here-str-pos))
+
+          ; Reading the ending here-string marker,
+          ; and the current character is correct.
+          (and (neg? here-str-pos)
+               (< (- here-str-pos) here-marker-len)
+               (= c (nth here-end (- here-str-pos))))
+            (recur
+              body-accum
+              (str-accum-push str-accum c)
+              leading-ws
+              brace-level
+              (dec here-str-pos))
+
+          ; Unexpected EOF
+          (nil? c)
+            (reader-error reader "Unexpected EOF while reading a body part")
+
+          ; Newline encountered: dump accumulator,
+          ; turn the leading whitespace mode on
+          (= c \newline)
+            (let [body-accum
+                   (-> body-accum
+                     (dump-string str-accum)
+                     append-newline)]
+              (recur
+                body-accum
+                (make-str-accum)
+                true
+                brace-level
+                (int 0)))
+
+          ; In leading whitespace mode, a whitespace character encountered
+          (and (whitespace? c) leading-ws)
+            (recur
+              body-accum
+              (str-accum-push str-accum c)
               true
               brace-level
-              (int 0)))
+              (int 0))
 
-        ; in leading whitespace mode, whitespace character encountered
-        (and (whitespace? c) leading-ws)
-          (recur
-            body-accum
-            (str-accum-push str-accum c)
-            true
-            brace-level
-            (int 0))
-
-        :else
-          (recur
-            (dump-nonws-char-body body-accum str-accum leading-ws)
-            (dump-nonws-char-str str-accum c leading-ws)
-            false
-            brace-level
-            (int 0))
-
-        )))))
+          ; A normal character or a whitespace character
+          ; out of the leading whitespace mode
+          :else
+            (recur
+              (dump-nonws-char-body body-accum str-accum leading-ws)
+              (dump-nonws-char-str str-accum c leading-ws)
+              false
+              brace-level
+              (int 0)))))))
 
 (defn- validate-here-str
   "Checks that `here-str` does not contain escape-start/end chars,
@@ -238,6 +273,9 @@
            forms-present false]
       (let [c (reader-methods/read-1 reader)]
         (cond
+
+          ; Simple escaped string, e.g. the one starting with just
+          ; escape start char + body start char
           (and (= c escape-start-char)
                (= (reader-methods/peek reader) body-start-char))
             (do
@@ -245,6 +283,8 @@
               (recur
                 (conj forms-read (read-body-part settings reader ""))
                 true))
+
+          ; An escaped string with non-empty here-string
           (= c escape-start-char)
             (let [here-str (read-until reader body-start-char)
                   next-c (reader-methods/read-1 reader)]
@@ -255,18 +295,24 @@
                   (recur
                     (conj forms-read body-part)
                     true))))
+
+          ; A simple body part
           (= c body-start-char)
             (recur
               (conj forms-read (read-body-part settings reader nil))
               true)
+
+          ; A datum part
           (= c datum-start-char)
             (let [forms (reader-methods/read-delimited-list
                            datum-end-char reader)]
               (recur (vec (concat forms-read forms)) true))
+
           (nil? c)
             (if forms-present
               (list* forms-read)
               reader)
+
           :else
             (do
               (reader-methods/unread reader c)
@@ -311,11 +357,16 @@
         comment-char (.comment-char settings)
         c (reader-methods/read-1 reader)]
     (cond
+
+      ; No command, body or datum starts right away.
+      ; Pass the execution to the parts reader.
       (or (= c body-start-char)
           (= c datum-start-char))
         (do
           (reader-methods/unread reader c)
           (read-parts settings reader))
+
+      ; A comment form
       (= c comment-char)
         (let [next-c (reader-methods/peek reader)]
           (condp = next-c
@@ -325,19 +376,26 @@
           ; By convention, if the reader function has read nothing,
           ; it returns the reader.
           reader)
+
       (whitespace? c)
         (reader-error reader
           "Unexpected whitespace at the start of a Scribble form")
+
       (nil? c)
         (reader-error reader
           "Unexpected EOF at the start of a Scribble form")
+
+      ; An escaped (spliced) form, or an escaped string.
       (= c escape-start-char)
         (let [next-c (reader-methods/peek reader)]
           (if (= next-c escape-start-char)
             (read-parts settings reader)
+            ; If it is a spliced form, mark it with metadata,
+            ; so that it could be spliced in the parent reader.
             (mark-for-splice
               (reader-methods/read-delimited-list
                 escape-end-char reader))))
+
       :else
         (do
           (reader-methods/unread reader c)
